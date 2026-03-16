f5e3f0959dd7540338533f0686078531fac6150e
Ashish Ezhava <ashishezhava@gmail.com>
2026-03-17 00:19:07 +0530

    Optimised the speed for the landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://dctechnolabs.in',
+  trailingSlash: 'always',
   compressHTML: true,
   build: {
-    inlineStylesheets: 'auto'
+    inlineStylesheets: 'always'
   }
 });
